1679ca589d43f9c74f04ba994e67a225f630a3d9
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2019-01-06 16:54:09 +0100

    setup

---
+++ API/Entities/DataContext/AuctionContext.cs
@@ -0,0 +1,27 @@
+﻿using API.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities.DataContext
+{
+    public class AuctionContext : DbContext
+    {
+        public AuctionContext(DbContextOptions<AuctionContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<AuctionItem> AuctionItems { get; set; }
+
+        DbSet<Bid> Bids { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ API/Entities/Models/Bid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities.Models
+{
+    public class Bid
+    {
+        public int ItemNumber { get; set; }
+
+        public int Price { get; set; }
+
+        public string CustomName { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
